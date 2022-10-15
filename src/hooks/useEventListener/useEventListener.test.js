0acd7dcaef2dc4f05c856eae0ebb6ea621dbd870
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2022-10-15 14:52:40 +0100

    install userEvent package

--- src/hooks/useEventListener/useEventListener.test.js
+++ src/hooks/useEventListener/useEventListener.test.js
@@ -1,11 +1,13 @@
 import React, { useRef } from "react";
-import { render, screen, userEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import { useEventListener } from "./useEventListener";
 
 describe("useEventListener", () => {
   const TestComponent = ({ event, callback, element }) => {
     const buttonRef = useRef();
-    const el = element || buttonRef;
+    const el = element || buttonRef.current;
+    console.log(el);
     useEventListener(event, callback, el);
 
     return <button ref={buttonRef} />;
