209abac75d03dd67af65ec488c0fe6d0bfccf0ac
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2022-10-15 14:45:43 +0100

    write tests for useEventListener

---
+++ src/hooks/useEventListener/useEventListener.test.js
@@ -0,0 +1,34 @@
+import React, { useRef } from "react";
+import { render, screen, userEvent } from "@testing-library/react";
+import { useEventListener } from "./useEventListener";
+
+describe("useEventListener", () => {
+  const TestComponent = ({ event, callback, element }) => {
+    const buttonRef = useRef();
+    const el = element || buttonRef;
+    useEventListener(event, callback, el);
+
+    return <button ref={buttonRef} />;
+  };
+
+  const defaultProps = {
+    event: 'click',
+    callback: jest.fn(),
+  }
+
+  it("successfully adds an event listener to an element", () => {
+    render(<TestComponent {...defaultProps} />);
+    const button = screen.getByRole('button');
+
+    userEvent.click(button);
+    expect(defaultProps.callback).toHaveBeenCalledTimes(1);
+  });
+
+  it("does not add an event listener if the element passed is invalid", () => {
+    render(<TestComponent {...defaultProps} element="invalid" />);
+    const button = screen.getByRole('button');
+
+    userEvent.click(button);
+    expect(defaultProps.callback).not.toHaveBeenCalled();
+  });
+});
